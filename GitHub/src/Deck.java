3341d32f6a771e49d09a4afea42fd1e8bc688b96
AppsDaniel <apps.DanielLarsson@gmail.com>
2017-10-11 14:28:23 +0200

    Början av ArrayList

--- GitHub/src/Deck.java
+++ GitHub/src/Deck.java
@@ -1,15 +1,26 @@
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Deck {
 
-	// private ArrayList<Deck> cards;
+
 	String suit[] = { "DIAMOND", "HEART", "SPADE", "CLUB" };
 	String values[] = { "TWOO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN",
 			"KING", "ACE" };
+	
+	ArrayList<String> newList = new ArrayList<>();
 
 	Random random = new Random();
+	
+	protected void newCard3() {
+			for (int s = 0; s < suit.length; s++) {
+				for (int v = 0; v < values.length; v++) {
+					String aNewCard = values[v]+ " "+ suit[s];
+					System.out.println(aNewCard);
+				}	
+			}
+	}
 
 	protected String createSuit() {
 		String suits = suit[random.nextInt(suit.length)];
